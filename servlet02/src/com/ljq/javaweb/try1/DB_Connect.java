fce2a1f4f16232a36f7e87f03c20344cebac8f01
lalala129 <stevenleiqi@163.com>
2022-04-04 00:07:52 +0800

    将连接JDBC方式改为写死

--- servlet02/src/com/ljq/javaweb/try1/DB_Connect.java
+++ servlet02/src/com/ljq/javaweb/try1/DB_Connect.java
@@ -9,6 +9,7 @@ import java.util.Properties;
 
 public class DB_Connect {
     public Connection connect(String path) throws SQLException, ClassNotFoundException, IOException {
+        /*
         FileInputStream in = new FileInputStream(path);
         Properties prop = new Properties();
         prop.load(in);
@@ -16,6 +17,11 @@ public class DB_Connect {
         String my_url=prop.getProperty("my_url");
         String my_user=prop.getProperty("my_user");
         String my_password=prop.getProperty("my_password");
+        */
+        String driver="com.mysql.cj.jdbc.Driver";
+        String my_url="jdbc:mysql://127.0.0.1:3306/netdiskdata";
+        String my_user="root";
+        String my_password="lei003427";
         Class.forName(driver);
         return DriverManager.getConnection(my_url, my_user, my_password);
     }
