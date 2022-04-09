61c21b2d2eac0d147dc27fff63a4df40483f3971
lalala129 <stevenleiqi@163.com>
2022-04-09 17:37:03 +0800

    更正中文乱码的问题

--- servlet02/src/com/ljq/javaweb/try1/DB_Connect.java
+++ servlet02/src/com/ljq/javaweb/try1/DB_Connect.java
@@ -18,7 +18,7 @@ public class DB_Connect {
 */
         //需要把此处信息改为自己的
         String driver = "com.mysql.cj.jdbc.Driver";
-        String my_url = "jdbc:mysql://127.0.0.1:3306/netdiskdata";
+        String my_url = "jdbc:mysql://127.0.0.1:3306/netdiskdata?useUnicode=true&characterEncoding=utf-8";
         String my_user = "root";
         String my_password = "lei003427";
         Class.forName(driver);
