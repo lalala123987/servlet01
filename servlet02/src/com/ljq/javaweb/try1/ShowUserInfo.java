86e936b30d4b425294520ff2fd68039372bd9cdd
lalala129 <stevenleiqi@163.com>
2022-04-09 17:52:59 +0800

    修正注册登录中中文乱码的问题

--- servlet02/src/com/ljq/javaweb/try1/ShowUserInfo.java
+++ servlet02/src/com/ljq/javaweb/try1/ShowUserInfo.java
@@ -11,6 +11,7 @@ public class ShowUserInfo extends ViewBaseServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, UnsupportedEncodingException {
         request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
         HttpSession session = request.getSession(false);//TODO:不创建ID,尚不知是否可以伪造ID，如果能，可绕过
         //if (!session.isNew()){//TODO 此处需要删除程序才能运行，但是删除后会有安全隐患，可以伪造refer后再使用新session并使用post直接访问即可修改数据
         Referer_Check RC = new Referer_Check(request.getHeader("referer"),"/servlet02/login.html");
@@ -26,15 +27,10 @@ public class ShowUserInfo extends ViewBaseServlet {
                         e.printStackTrace();
                     }
                     super.processTemplate("UserInfo",request,response);
-                    System.out.println("arrive");
                 }
                 else { //TODO:实际已登录用户也无法到达此界面，因为需要通过SUI的渲染
-                    System.out.println("Username"+session.getAttribute("username"));
-                    System.out.println("getUN:"+request.getParameter("username"));
                     if (session.getAttribute("username").equals(request.getParameter("username"))){
-                        System.out.println("10");
                         super.processTemplate("UserInfo",request,response);
-                        System.out.println("arrive");
                     }
                     else response.sendRedirect("403.html");
                 }
